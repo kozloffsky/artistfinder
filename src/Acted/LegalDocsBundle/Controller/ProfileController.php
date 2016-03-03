435a3c770105b55c06753f36ef7d4472d4a8751a
dexm <dexm238@gmail.com>
2016-03-03 12:00:26 +0200

    added get param for authenticated user

--- src/Acted/LegalDocsBundle/Controller/ProfileController.php
+++ src/Acted/LegalDocsBundle/Controller/ProfileController.php
@@ -4,13 +4,19 @@ namespace Acted\LegalDocsBundle\Controller;
 
 use Acted\LegalDocsBundle\Entity\Artist;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProfileController extends Controller
 {
-    public function showAction($slug)
+    public function showAction(Request $request, $slug)
     {
         $em = $this->getDoctrine()->getManager();
+
+        //TODO: for debug only
+        $user = ($request->get('user'))
+            ? $em->getRepository('ActedLegalDocsBundle:User')->findOneById($request->get('user')) : null;
+
 
         /** @var Artist $artist */
         $artist = $em->getRepository('ActedLegalDocsBundle:Artist')->findOneBySlug($slug);
@@ -19,9 +25,9 @@ class ProfileController extends Controller
             throw new NotFoundHttpException();
         }
 
-        return $this->render('ActedLegalDocsBundle:Profile:show.html.twig', array(
-            'artist' => $artist,
-        ));
+        return $this->render('ActedLegalDocsBundle:Profile:show.html.twig',
+            compact('artist', 'user')
+        );
     }
 
     public function listAction()
