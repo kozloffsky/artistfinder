6404b8565313bd0f109064204aadfade61521825
dexm <dexm238@gmail.com>
2016-03-09 16:27:17 +0200

    changed categories title length

--- src/Acted/LegalDocsBundle/DataFixtures/ORM/LoadCategoryData.php
+++ src/Acted/LegalDocsBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -47,11 +47,11 @@ class LoadCategoryData extends AbstractFixture implements ContainerAwareInterfac
         $varietyActsCategory->setImage('assets/images/variety_acts.png');
 
         $subCategory1 = new Category();
-        $subCategory1->setTitle($faker->unique()->text(40));
+        $subCategory1->setTitle($faker->unique()->text(10));
         $subCategory1->setParent($varietyActsCategory);
 
         $subCategory2 = new Category();
-        $subCategory2->setTitle($faker->unique()->text(40));
+        $subCategory2->setTitle($faker->unique()->text(10));
         $subCategory2->setParent($varietyActsCategory);
 
         $internationalArtists = new Category();
@@ -59,11 +59,11 @@ class LoadCategoryData extends AbstractFixture implements ContainerAwareInterfac
         $internationalArtists->setImage('assets/images/international.png');
 
         $subCategory3 = new Category();
-        $subCategory3->setTitle($faker->unique()->text(40));
+        $subCategory3->setTitle($faker->unique()->text(10));
         $subCategory3->setParent($internationalArtists);
 
         $subCategory4 = new Category();
-        $subCategory4->setTitle($faker->unique()->text(40));
+        $subCategory4->setTitle($faker->unique()->text(10));
         $subCategory4->setParent($internationalArtists);
 
         $manager->persist($varietyActsCategory);
