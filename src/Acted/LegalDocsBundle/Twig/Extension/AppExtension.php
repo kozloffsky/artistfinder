0c549082659dd2db08baa08bf06202911628342d
dexm <dexm238@gmail.com>
2016-03-09 13:12:59 +0200

    added category as twig extension

---
+++ src/Acted/LegalDocsBundle/Twig/Extension/AppExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acted\LegalDocsBundle\Twig\Extension;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppExtension extends \Twig_Extension
+{
+
+    /** @var  ObjectManager */
+    private $manager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'app';
+    }
+
+    public function setEntityManager(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            'categories' => new \Twig_SimpleFunction('categories', [$this, 'renderCategories'], [
+                'is_safe' => ['html'],
+                'needs_environment' => true,
+            ])
+        ];
+    }
+
+    public function renderCategories(\Twig_Environment $twig)
+    {
+        $categoriesRepo = $this->manager->getRepository('ActedLegalDocsBundle:Category');
+        $categories = $categoriesRepo->childrenHierarchy();
+        return $twig->render('ActedLegalDocsBundle:Default:categories.html.twig', compact('categories'));
+    }
+}
