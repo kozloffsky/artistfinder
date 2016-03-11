eacd3b7b490f732dfe544a51632dead1addfe81d
dexm <dexm238@gmail.com>
2016-03-11 11:15:56 +0200

    added Performance form

---
+++ src/Acted/LegalDocsBundle/Form/PerformanceType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acted\LegalDocsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PerformanceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('techRequirement')
+            ->add('profile')
+            ->add('media')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acted\LegalDocsBundle\Entity\Performance',
+            'allow_extra_fields' => true,
+            'csrf_protection'   => false,
+        ));
+    }
+}
