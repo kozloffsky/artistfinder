fd7c5ddffba6c89dd8be93f70bfeb0e78000b6d6
dexm <dexm238@gmail.com>
2016-03-14 12:17:16 +0200

    added Imagine Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new KnpU\GuardBundle\KnpUGuardBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
