6427e88e92c953909683acdd1cb1d770d3dd91bc
dexm <dexm238@gmail.com>
2016-03-22 14:08:16 +0200

    added Resetting Password Form; added Resetting Password email template; added Change Password template;

---
+++ src/Acted/LegalDocsBundle/Form/ResettingFormType.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dexm1
+ * Date: 22.03.16
+ * Time: 13:48
+ */
+
+namespace Acted\LegalDocsBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ResettingFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('password', RepeatedType::class, [
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Password confirmation'],
+            'constraints' => [new NotBlank(), new Length(['min' => 6])]]
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'allow_extra_fields' => true,
+        ]);
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return null;
+    }
+
+}
