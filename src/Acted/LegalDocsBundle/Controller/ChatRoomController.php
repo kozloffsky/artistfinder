fc0de99a529b3b4124e4b909242cbae1481a570f
jerry <trogvar22@gmail.com>
2016-05-12 19:17:47 +0300

    add controller and rout

---
+++ src/Acted/LegalDocsBundle/Controller/ChatRoomController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Acted\LegalDocsBundle\Controller;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+
+class ChatRoomController extends Controller
+{
+
+}
