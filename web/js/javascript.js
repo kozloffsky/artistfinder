d837b24668b669b0f469fba5048c44ab529fa412
pavel <ibanezgsr205>
2016-06-06 19:42:02 +0300

    M5 front

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -3002,7 +3002,70 @@ $(function() {
             type:'GET',
             url:'/event/change_status/reject/'+id+'?type=no_email',
             success: function(res){
-                //$('article #'+id).
+                //$('article#'+id).
+            }
+        })
+    }
+
+    $('.archiveMessage').on('click',function(){
+        var messageId = $(this).parents('article').attr('id');
+        archiveMessage(messageId)
+    })
+
+    function archiveMessage(id){
+        console.log(id)
+        $.ajax({
+            type:'POST',
+            url:'/dashboard/archived/message/'+id,
+            success: function(res){
+                $('article#'+id).fadeOut();
+            }
+        })
+    }
+
+    $('#sortMessages').on('change',function(){
+        var filterValueSelected = $(this).find('option:selected').val();
+        messagesFilter(filterValueSelected)
+    });
+
+    function messagesFilter(val){
+        var userId = $('header #userInformation').text();
+        console.log(val)
+        $.ajax({
+            type:'GET',
+            url:'/dashboard/filter/messages/'+userId+'?filters='+val,
+            success: function(res){
+                $('.messages .dialogs').empty();
+                $(res).each(function(){
+                    var viewMessages = '<article class="col-xs-12 dialog-section noselect" data-longtap-duration="500" id="'+this.id+'">'+
+                        '<div class="wrap clearfix">'+
+                        '<div class="avatar">'+
+                        '<img src="'+this.sender_user.avatar+'" alt=""/>'+
+                        '</div>'+
+                        '<div class="user-info">'+
+                        '<span class="user-name">'+this.sender_user.firstname+'</span>'+
+                        '<span class="time">Today at 11:34 pm</span>'+
+                        '</div>'+
+                        '<div class="message-block">'+
+                        '<div class="col-lg-10 col-sm-10 text-block">'+
+                        '<p class="message-heading hidden-xs">Enquiry for '+this.chat_room.event.title+' / '+this.chat_room.event.city.name+'/ '+this.chat_room.event.starting_date+'</p>'+
+                        '<div class="text">'+this.message_text+'</div>'+
+                        '<div class="controls hidden-xs">'+
+                        '<div class="button-gradient filled blue">'+
+                        '<a href="/chat-room" class="btn register">See Conversation</a>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'+
+                        '<div class="col-lg-2 col-sm-2  message-controls no-pad">'+
+                        '<ul class="ul-reset">'+
+                        '<li class="status confirmed"><i class="confirmed"></i> '+this.chat_room.offer.event_offer[0].status+'</li>'+
+                        '</ul>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'+
+                        '</article>';
+                    $('.messages .dialogs').append(viewMessages);
+                })
             }
         })
     }
