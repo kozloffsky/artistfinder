747cc415dd60298c823058fe20539cce394a4d54
pavel <ibanezgsr205>
2016-07-15 17:35:07 +0300

    fix performanc Iframe size

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -10157,7 +10157,7 @@ $(function() {
                             $.ajax({
                                 type: "POST",
                                 url: '/media/' + mediaId + '/edit',
-                                data: {"file": resp},
+                                data: {"file": resp,"position":2},
                                 beforeSend: function () {
                                     $('#loadSpinner').fadeIn(500);
                                 },
@@ -10810,8 +10810,23 @@ $(function() {
     function getPagination(paginationRoute, paginationTarget){
         $.get(paginationRoute, function( data ) {
             $(paginationTarget).html(data);
+            setPerformanceFrameHeight();
         });
     }
+
+    setPerformanceFrameHeight();
+
+    $( window ).resize(function() {
+        setPerformanceFrameHeight();
+    });
+
+    function setPerformanceFrameHeight(){
+        $('.price-list article:not(.newPerformanceBlank)').each(function(){
+            var firstHolderSize = $(this).find('.holder')[0].clientHeight;
+            $(this).find('iframe').css('height', firstHolderSize);
+        });
+    }
+
     iframeTracking();
 
     function iframeTracking(){
