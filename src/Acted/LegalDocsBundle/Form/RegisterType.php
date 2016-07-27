23cd78ec237a1ffb0b79ea2fa2a5efeefa45cdf8
jerry <trogvar22@gmail.com>
2016-07-27 10:39:02 +0300

    change type of field

--- src/Acted/LegalDocsBundle/Form/RegisterType.php
+++ src/Acted/LegalDocsBundle/Form/RegisterType.php
@@ -90,11 +90,8 @@ class RegisterType extends AbstractType
                 'description' => 'City ID (for ROLE_ARTIST)',
                 'required' => false
             ])
-            ->add('fake', ChoiceType::class, [
-                'choices_as_values' => true,
-                'choices' => [false, true],
-                'empty_data' => false,
-                'required' => false
+            ->add('fake', CheckboxType::class, [
+                'required' => false,
             ])
         ;
 
