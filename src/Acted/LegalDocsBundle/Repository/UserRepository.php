4c788109d8a96f8b9631233feb936c1f7e553c6e
jerry <trogvar22@gmail.com>
2016-09-13 18:45:33 +0300

    add fulltext index

--- src/Acted/LegalDocsBundle/Repository/UserRepository.php
+++ src/Acted/LegalDocsBundle/Repository/UserRepository.php
@@ -30,7 +30,9 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
 
         if ($query) {
             $qb
-                ->andWhere('(MATCH(a.name, a.assistantName) AGAINST (:query BOOLEAN) > 0)')
+                ->andWhere('(MATCH(a.name, a.assistantName) AGAINST (:query BOOLEAN) > 0
+                            OR MATCH(u.firstname, u.lastname) AGAINST (:query BOOLEAN) > 0)
+                ')
                 ->setParameter('query', $query);
         }
         switch ($fake) {
