b49e559d2973f215217baf78dc91e35f3c43a422
George Ivlev <george@lanars.com>
2016-11-01 10:29:48 +0200

    Restore files. 01/11/2016 10:29 AM

--- src/Acted/LegalDocsBundle/Resources/public/js/quotation.js
+++ src/Acted/LegalDocsBundle/Resources/public/js/quotation.js
@@ -1,3 +1,20 @@
-/**
- * Created by george on 31.10.16.
- */
+;(function(window) {
+
+    function openQuotationModal() {
+        $("#quotationModal").modal('show');;
+    }
+
+    $(".enquiries .quotationSendbtn").on("click", openQuotationModal);
+
+    // $.ajax({
+    //     url: "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=37.530101,38.600062&zoom=14&size=640x400&key=AIzaSyDLK8SupBcU-H0H0SF0PIar5UP-y-DCrTI ",
+    //     method: "GET",
+    //     success: function(res) {
+    //         console.log(res);
+    //     }
+    // });
+
+
+
+
+})(this);
