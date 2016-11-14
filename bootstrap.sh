8ef4f6decbe84b39842bed33a74b51f7b1410504
George Ivlev <george@lanars.com>
2016-11-14 13:01:34 +0200

    Update Vagrant configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,23 +10,28 @@ echo "deb-src http://packages.dotdeb.org jessie all" | sudo tee -a /etc/apt/sour
 wget -q https://www.dotdeb.org/dotdeb.gpg
 sudo apt-key add dotdeb.gpg
 
-#echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.6" | sudo tee -a /etc/apt/sources.list.d/mysql.list
-#sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
+echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.6" | sudo tee -a /etc/apt/sources.list.d/mysql.list
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 
 sudo apt-get update
+sudo apt-get install -y debconf-utils
 
-#echo -e "Setting up MySQL Server...\n"
-#export DEBIAN_FRONTEND="noninteractive"
-#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
-#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
-#sudo apt-get install -y mysql-server-5.6
-#mysql_secure_installation
-#sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+echo -e "Setting up MySQL Server...\n"
+export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $DBPASSWD"
 
-#echo -e "Setting up MySQL user and DB...\n"
-#mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON *.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBPASSWD' WITH GRANT OPTION;"
-#mysql -uroot -p$DBPASSWD -e "FLUSH PRIVILEGES;"
-#/etc/init.d/mysql restart
+sudo apt-get -y install mysql-server-5.6
+mysql_secure_installation
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+
+# echo -e "Setting up MySQL user and DB...\n"
+# mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON *.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBPASSWD' WITH GRANT OPTION;"
+# mysql -uroot -p$DBPASSWD -e "FLUSH PRIVILEGES;"
+/etc/init.d/mysql restart
 
 echo -e "Setting up Git, MC, PHP5, Xdebug and Composer...\n"
 apt-get -y install git mc curl php5 php5-cli php5-curl php5-gd php5-fpm php5-intl php5-mcrypt php5-mysql php5-xdebug php5-dev php-pear php5-imagick imagemagick
@@ -36,8 +41,6 @@ echo "xdebug.remote_enable=1" | sudo tee -a /etc/php5/fpm/conf.d/20-xdebug.ini
 echo "xdebug.remote_host=10.0.2.2" | sudo tee -a /etc/php5/fpm/conf.d/20-xdebug.ini
 echo "xdebug.remote_port=9000" | sudo tee -a /etc/php5/fpm/conf.d/20-xdebug.ini
 /etc/init.d/php5-fpm restart
-
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 echo -e "Setting up Nginx...\n"
 apt-get -y install nginx
@@ -55,3 +58,9 @@ echo -e "Setting up app/cache and app/logs permissions...\n"
 mkdir /vagrant/app/{cache,logs}
 chmod 777 -R /vagrant/app/cache/
 chmod 777 -R /vagrant/app/logs/
+chmod 777 -R /vagrant/app/web/images
+
+echo -e "Setting up a composer + vendor dependencies...\n"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+cd /vagrant
+composer install --ignore-platform-reqs
