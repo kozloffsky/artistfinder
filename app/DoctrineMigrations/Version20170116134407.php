71a66885b255e8bb51cc1dee2adc347312978142
George Ivlev <george@lanars.com>
2017-01-16 16:00:25 +0200

    Remove nullable from city id. 16/01/2017 4:00 PM

--- app/DoctrineMigrations/Version20170116134407.php
+++ app/DoctrineMigrations/Version20170116134407.php
@@ -18,7 +18,7 @@ class Version20170116134407 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Event ADD place_id VARCHAR(256) DEFAULT NULL, CHANGE city_id city_id INT NOT NULL');
+        $this->addSql('ALTER TABLE Event ADD place_id VARCHAR(256) DEFAULT NULL');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20170116134407 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Event DROP place_id, CHANGE city_id city_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE Event DROP place_id');
     }
 }
